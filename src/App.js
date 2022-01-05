a0455dc22beb619fda740b2a37f142f1b2fa2aae
HHJ121 <dandylioneffect@gmail.com>
2022-01-05 16:22:11 -0600

    Installed React, Axios and added background image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Loader from "react-loader-spinner";
+import axios from "axios";
+
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-background">
+        <h1>Hello world</h1>
+        <Weather />
+        <Loader
+          type="BallTriangle"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={3000} //3 secs
+        />
+      </div>
     </div>
   );
 }
