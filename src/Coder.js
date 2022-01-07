f77a64e2e2d9a96d0633724f6feb86da3ea4369e
HHJ121 <dandylioneffect@gmail.com>
2022-01-07 16:43:48 -0600

    Fixed links

--- src/Coder.js
+++ src/Coder.js
@@ -6,11 +6,19 @@ export default function Coder() {
   return (
     <div className="Coder">
       <p>
-        <a href="https://github.com/HHJ121/weather-react-app" target="_blank">
+        <a
+          href="https://github.com/HHJ121/weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           Open-source code
         </a>{" "}
         by Haw-Harn Jiang, hosted on{" "}
-        <a href="https://cocky-elion-151f36.netlify.app/" target="_blank">
+        <a
+          href="https://cocky-elion-151f36.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Netlify
         </a>
       </p>
